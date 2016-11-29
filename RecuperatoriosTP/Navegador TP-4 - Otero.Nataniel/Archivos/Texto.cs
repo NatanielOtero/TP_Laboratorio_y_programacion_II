ec024e2fedf66e507ccfca5022bfcc2db6efbdc0
NatanielOtero <natanielotero@gmail.com>
2016-11-29 13:58:13 -0300

    Tp4 terminado

---
+++ RecuperatoriosTP/Navegador TP-4 - Otero.Nataniel/Archivos/Texto.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _archivo;
+
+        public Texto(string archivo)
+        {
+            this._archivo = archivo;
+        }
+        /// <summary>
+        /// guarda los datos en el archivo
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool guardar(string datos)
+        {
+            try
+            {
+                StreamWriter escritor  = new StreamWriter(this._archivo, true);
+                escritor.WriteLine(datos);
+                escritor.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                
+                
+                return false;
+            } 
+            
+        }
+        /// <summary>
+        /// Lee los datos del archivo y los asigna a datos
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool leer(out List<string> datos)
+        {
+            bool flag = false;
+            try
+            {
+                StreamReader lector = new StreamReader(this._archivo);
+
+                datos = new List<string>();
+
+                while(!lector.EndOfStream)
+                {
+                    datos.Add(lector.ReadLine());
+                }
+
+                lector.Close();
+
+                flag = true;
+            }
+            catch (Exception)
+            {
+                datos = null;
+                flag = false;
+            }            
+             return flag;
+            
+        }
+    }
+}
---
+++ RecuperatoriosTP/Navegador TP-4 - Otero.Nataniel/Hilo/Descargador.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+        public delegate void _progresoEvento(int progreso);        
+        public delegate void _finDescargaEvento(string html);
+        public event _progresoEvento ProgresoDescarga;
+        public event _finDescargaEvento FinDescarga;
+
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        /// <summary>
+        /// actualiza la barra de progreso
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.ProgresoDescarga(e.ProgressPercentage);
+        }
+        /// <summary>
+        /// carga el html con el resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.html = e.Result;
+
+            }
+            catch (Exception)
+            {
+
+                this.html = "Error en la descarga";
+            }
+            finally 
+            {
+                this.FinDescarga(html);
+            }
+            
+            
+        }
+    }
+}
